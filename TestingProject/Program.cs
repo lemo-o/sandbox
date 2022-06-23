fa04f56c666717f1c74e60016a64f1dd78a4acae
lemo-o <21benjaminhall@gmail.com>
2022-06-23 12:49:39 -0600

    Classes in C# + some errorchecking + plans for future errorchecking

--- TestingProject/Program.cs
+++ TestingProject/Program.cs
@@ -9,47 +9,88 @@ namespace seleniumtest
 {
     class FirstCSClass
     {
+        public static ChromeDriver driver;
+        static void browserStart(string temp) //also navigates to page
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(temp);
+            driver.Manage().Window.Maximize();
+          //  driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+        }
+        static void dropdownHandler(string x, string y /*, string l2, string l3 */) //input the xml for a dropdown and their respective codes 
+        {
+            Actions action = new Actions(driver);
+
+            var dropdown = driver.FindElement(By.XPath(x));
+
+            action.MoveToElement(dropdown).Perform(); //xpath doesn't work for something reason? use the id - ID doesn't work? It's probably a clicking box; therefore, click the box, then check for xpath again
+            try
+            {
+                var level_1 = driver.FindElement(By.XPath(y));
+                level_1.Click();
+            }
+            catch
+            {
+                try
+                {
+                    var level_1 = driver.FindElement(By.Id(y));
+                    level_1.Click();
+                }
+                catch
+                {
+                    dropdown.Click();
+                    var level_1 = driver.FindElement(By.XPath(y));
+                    level_1.Click();
+                }
+            }
+
+            /* Logic for this should be:
+             * Check what is the "lowest" level entered
+             * Navigate to the one before that level
+             * Click on the lowest level
+             */
+
+            //this will probably need a pretty good waiting strategy. As of now, I wait implicitly in the main section of the code
+        }
+        static void textboxHandler(string location, string text) //this section needs errorchecking
+        {
+            var temp = driver.FindElement(By.XPath(location));
+            temp.SendKeys(text);
+        }
+        static void loginPage(string login, string pw) //this can 100% be cleverer - current it's not "generalized" Past that, it lacks errorchecking
+        {
+            var username = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div/form/div[2]/input"));
+            username.SendKeys(login);
+
+            var password = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div/form/div[3]/input"));
+            password.SendKeys(pw);
+
+            password.Submit();
+        }
         static void Main(string[] args)
         {
-            var driver = new ChromeDriver(); //Initialize
 
-            driver.Navigate().GoToUrl("https://brokerage.qc.suntecktts.com/web/home");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+            
 
-            var username = driver.FindElement(By.Id("agent_login"));
-            username.SendKeys("ben.hall");
+            browserStart("https://brokerage.qc.suntecktts.com/agents/login");
+            loginPage("-", "-");
+            dropdownHandler("/html/body/header/nav/div/div/div[3]/ul/li[3]", "/html/body/header/nav/div/div/div[3]/ul/li[3]/ul/li[2]/a"); //navigation to the CreateTL form
+            dropdownHandler("/html/body/div[2]/div[2]/form/div[2]/div[2]/div/div[1]/div[1]/div/div/span/span[1]/span/span[2]", "/html/body/span[2]/span/span[2]/ul/li[2]");
 
-            var password = driver.FindElement(By.Id("agent_password"));
-            password.SendKeys("Qll2mm.08");
-            password.Submit();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(100);
-
-            var Loads = driver.FindElement(By.XPath("/html/body/header/nav/div/div/div[3]/ul/li[3]/a"));
-            var createTL = driver.FindElement(By.XPath("/html/body/header/nav/div/div/div[3]/ul/li[3]/ul/li[2]/a"));
-
-            Actions actions = new Actions(driver);
-
-            actions.MoveToElement(Loads).Perform();
-            createTL.Click();
+            
+            dropdownHandler("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/div[1]/div/div/div[1]/div/span", "/html/body/span[2]/span/span[2]/ul/li[2]"); //shipper
+            dropdownHandler("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/div[2]/div/div/div[1]/div/span/span[1]/span/span[1]", "/html/body/span[2]/span/span[2]/ul/li[1]"); //consignnee
+            textboxHandler("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/div[1]/div/div/div[13]/div[1]/div/input", "6/24/2022"); //this should ideally use a function of Time
+            textboxHandler("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/div[2]/div/div/div[13]/div[1]/div/input", "6/24/2022"); //this too
+            
+            textboxHandler("/html/body/div[2]/div[2]/form/div[4]/div[2]/div/div/div[1]/div[2]/div/input", "1"); //quantity, should be random, from 1-10000
+            dropdownHandler("/html/body/div[2]/div[2]/form/div[4]/div[2]/div/div/div[1]/div[3]/div/span/span[1]/span", "/html/body/span[2]/span/span[2]/ul/li[1]"); //UoM, should be randomized for what is available in the dropdown
+            textboxHandler("/html/body/div[2]/div[2]/form/div[4]/div[2]/div/div/div[1]/div[4]/div/input", "1"); //Description, should fill with random garbage
+            textboxHandler("/html/body/div[2]/div[2]/form/div[4]/div[2]/div/div/div[1]/div[5]/div/input", "1"); //Weight, should fill with a random value 1-150,000
 
 
-            /*           var title = driver.Title;
-                       Assert.AreEqual("Google", title);
 
-                       driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
-
-                       var searchBox = driver.FindElement(By.Name("q"));
-                       var searchButton = driver.FindElement(By.Name("btnK"));
-
-                       searchBox.SendKeys("Selenium");
-                       searchButton.Click();
-
-                       searchBox = driver.FindElement(By.Name("q"));
-                       var value = searchBox.GetAttribute("value");
-                       Assert.AreEqual("Selenium", value);
-
-                       driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(2500);
-                       driver.Quit(); */
         }
+
     }
 }
