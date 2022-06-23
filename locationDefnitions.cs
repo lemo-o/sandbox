11ae31ad31b4c05c35c745211c4369ce17f11c56
lemo-o <21benjaminhall@gmail.com>
2022-06-23 15:16:16 -0600

    Added locationDefinitions.cs outline

---
+++ locationDefnitions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace seleniumtest
+{
+	public class generalDefinitions
+	{
+		public string[] names =
+		{
+
+		};
+		public string[] address1 =
+		{
+
+		};
+		public string[] address2 =
+		{
+
+		};
+		public string[] cities =
+		{
+
+		};
+		public string[] states =
+		{
+
+		};
+		public string[] countries =
+		{
+
+		};
+		public string[] zipCodes =
+		{
+
+		};
+	}
+}
+
