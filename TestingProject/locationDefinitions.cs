ba13c66294c598df9b84684b82124021186b8329
lemo-o <21benjaminhall@gmail.com>
2022-06-23 21:07:48 -0600

    Added commas

--- TestingProject/locationDefinitions.cs
+++ TestingProject/locationDefinitions.cs
@@ -27,19 +27,19 @@ namespace seleniumtest
 
 		public string[5,] city_Blocks //two dimensional array to handle how locations are tied together - 0 is city, 1 is state, 2 is state abbreviation, 3 is country, 4 is country abbreviation, 5 is ZIP code
 		{
-            cityBlocks[0,0] = "Boise";
-			cityBlocks[1,0] = "Idaho";
-            cityBlocks[2,0] = "ID";
-            cityBlocks [3,0] = "United States";
-			cityBlocks [4,0] = "US";
-			cityBlocks [5,0] = "83701";
+            cityBlocks[0,0] = "Boise",
+			cityBlocks[1,0] = "Idaho",
+            cityBlocks[2,0] = "ID",
+            cityBlocks [3,0] = "United States",
+			cityBlocks [4,0] = "US",
+			cityBlocks [5,0] = "83701",
 
-            cityBlocks [0,1] =  "Oklahoma City";
-			cityBlocks [1,1] =  "Oklahoma";
-			cityBlocks [2,1] =  "OK";
-			cityBlocks [3,1] =  "United States";
-			cityBlocks [4,1] =  "US";
-			cityBlocks [5,1] =  "73116";
+            cityBlocks [0,1] =  "Oklahoma City",
+			cityBlocks [1,1] =  "Oklahoma",
+			cityBlocks [2,1] =  "OK",
+			cityBlocks [3,1] =  "United States",
+			cityBlocks [4,1] =  "US",
+			cityBlocks [5,1] =  "73116",
 		}; //If I was doing more than two locations, I would make this loop in such a way that I can enter: "Boise", "Idaho", "ID", "United States", "US", "83701" and let a function handle it
 
     }
